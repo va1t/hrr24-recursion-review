fea83b305521b00d55d82a4ade08a4eef897b1fa
va1t <peter.maxcy@gmail.com>
2017-05-15 23:15:30 -0400

    Upload Pairing Review

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // get doc body ie webpage
+  // function to iterate through child nodes
+  // use document.body, element.childNodes, and element.classList
+  var body = document.body;
+
+  var myArray = [];
+
+  function elementSearch(nodes){
+
+    if(nodes.classList && nodes.classList.contains(className)){
+      myArray.push(nodes);
+    }
+
+    for(var key in nodes.childNodes){
+      elementSearch(nodes.childNodes[key]);
+    }
+  }
+
+  elementSearch(body);
+
+  return myArray;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,44 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  //input of various things - objects, numbers, booleans, arrays, etc
+  //output: turn everything into a string, including [], {}
+  //
+  var holder = [];
+  var type = typeof obj;
+
+  if(type === 'number' || type === 'boolean') {
+    return obj.toString();
+  }
+  else if(type === 'string') {
+    return '\"' + obj + '\"';
+  }
+  else if(obj === 'undefined') {
+    return undefined
+  }
+  else if(obj === null) {
+    return 'null'
+  }
+  /*
+  else if(type === 'function') {
+    return ''
+  }*/
+
+  if(Array.isArray(obj)) {
+    obj.forEach(function(element, index) {
+      holder.push(stringifyJSON(element));
+    })
+    return "[" + holder.join(',') + "]"
+  }
+
+  else if(type === 'object') {
+    for(var key in obj) {
+      if(typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined'){
+        holder.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    return "{" + holder.join(',') + "}"
+  }
+
 };
